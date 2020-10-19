8b37773bbb7b716e2e92434309f69b0dc2b0dcf6
MonishVT1 <73113985+MonishVT1@users.noreply.github.com>
2020-10-19 21:41:30 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,38 @@
 #include <cstdlib>
 #include <cstdio>
 
+void swap(int *xp, int *yp) 
+{ 
+    int temp = *xp; 
+    *xp = *yp; 
+    *yp = temp; 
+} 
+  
+// An optimized version of Bubble Sort 
+void bubbleSort(int arr[], int n) 
+{ 
+   int i, j; 
+   bool swapped; 
+   for (i = 0; i < n-1; i++) 
+   { 
+     swapped = false; 
+     for (j = 0; j < n-i-1; j++) 
+     { 
+        if (arr[j] > arr[j+1]) 
+        { 
+           swap(&arr[j], &arr[j+1]); 
+           swapped = true; 
+        } 
+     } 
+  
+     // IF no two elements were swapped by inner loop, then break 
+     if (swapped == false) 
+        break; 
+   } 
+} 
+
+
+
 using namespace std;
 #define MAX 10000
 #define TRUE 1
